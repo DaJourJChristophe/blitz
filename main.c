551ddb2bc708885969f296c4a41232828e32c61f
DaJourJChristophe <dc198jcn215n@gmail.com>
2024-04-17 08:19:33 -0700

    update license

--- main.c
+++ main.c
@@ -1,3 +1,16 @@
+/**
+ * Academic Free License ("AFL") v. 3.0
+ *
+ * Copyright 2024 Da'Jour J. Christophe. All rights reserved.
+ *
+ * This Academic Free License (the "License") applies to any original
+ * work of authorship (the "Original Work") whose owner (the "Licensor")
+ * has placed the following licensing notice adjacent to the copyright
+ * notice for the Original Work:
+ *
+ * Licensed under the Academic Free License version 3.0.
+ */
+
 #include <stdlib.h>
 
 int main(void)
